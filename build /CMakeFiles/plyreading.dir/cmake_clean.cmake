b55be6ba25524fe715f77324fbef56cd65c0afd3
Maik <maik.khan28@gmail.com>
2019-02-04 16:29:28 +0100

    added build folder

---
+++ build /CMakeFiles/plyreading.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/plyreading.dir/plyreading.cpp.o"
+  "plyreading.pdb"
+  "plyreading"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/plyreading.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
